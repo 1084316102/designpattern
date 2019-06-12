6547af085ad31c750fc05d163cfac417c461be40
1084316102 <49268583+1084316102@users.noreply.github.com>
2019-06-12 14:39:24 +0800

    装饰

--- 装饰器模式/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        House house = new Department();
-        house = new Sofa(house);
-        System.out.println(house.room());
-        house = new Tv(house);
-        System.out.println(house.room());
-    }
-}
